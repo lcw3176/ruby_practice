2cfb4c37e0c7d1bcfe727885f6c0650d5806bce3
lcw3176 <lcw3176@naver.com>
2023-12-17 21:07:30 +0900

    스캐폴드 실습중

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_17_110941) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_120156) do
   create_table "books", force: :cascade do |t|
     t.string "isbn"
     t.string "title"
