e40ba43fb90f91191616d1391ed758a07a7c288f
lcw3176 <lcw3176@naver.com>
2023-12-17 20:23:54 +0900

    mvc 실습

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,4 +2,12 @@ class HelloController < ApplicationController
   def index
     render json: 'hello'
   end
+
+  def view
+    @msg = 'Hello World'
+  end
+
+  def list
+    @books = Book.all
+  end
 end
