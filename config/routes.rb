5dc6fbd1dd519fd321c85dad9e3cdc5c08c9ab9e
lcw3176 <lcw3176@naver.com>
2023-12-19 23:45:52 +0900

    프로젝트 리셋

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :fan_comments
-  resources :reviews
-  resources :authors
-  resources :users
-  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,6 +7,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  match ':controller(/:action(:/id))', via: [:get, :post, :patch]
 end
