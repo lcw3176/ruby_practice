b08066841602f57897c86ec0539ee0483eab5d84
comet <comet@daangn.com>
2024-01-05 11:47:30 +0900

    중고거래 컨트롤러, 조회 페이지 생성

--- app/controllers/fleamarket_article_controller.rb
+++ app/controllers/fleamarket_article_controller.rb
@@ -1,5 +1,13 @@
 class FleamarketArticleController < ApplicationController
   def index
+    @fleamarket_articles = FleamarketArticle.order(:updated_at).reverse.last(10)
+  end
 
+  def show
+    @article = FleamarketArticle.find(params[:id])
+    @article.read_count += 1
+    !@article.save
+
+    @like_count = FleamarketArticleLike.where(:fleamarket_article_id => params[:id]).count
   end
 end
