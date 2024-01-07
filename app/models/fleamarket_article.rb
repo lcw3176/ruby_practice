42b50a6d4edde996e6da69595946a0e0f572ec5d
comet <comet@daangn.com>
2024-01-07 17:16:39 +0900

    카테고리 enum 추가, default 값 설정

--- app/models/fleamarket_article.rb
+++ app/models/fleamarket_article.rb
@@ -3,5 +3,15 @@ class FleamarketArticle < ApplicationRecord
   has_many :fleamarket_article_likes
 
   enum :trade_status, [:done, :in_progress, :wait]
+  enum :category, [:life,
+                   :etc_used,
+                   :food,
+                   :digital_machine,
+                   :furniture,
+                   :child,
+                   :children_books,
+                   :sports,
+                   :books,
+                   :ticket]
 
 end
--- db/migrate/20240105015102_create_fleamarket_articles.rb
+++ db/migrate/20240105015102_create_fleamarket_articles.rb
@@ -7,9 +7,9 @@ class CreateFleamarketArticles < ActiveRecord::Migration[7.1]
       t.integer :price
       t.integer :category
       t.string :trade_address
-      t.string :thumbnail_url
-      t.integer :trade_status
-      t.integer :read_count
+      t.string :thumbnail_url, default: "https://avatars.githubusercontent.com/u/59993347?v=4"
+      t.integer :trade_status, default: FleamarketArticle.trade_statuses[:wait]
+      t.integer :read_count, default: 0
 
       t.timestamps
     end
