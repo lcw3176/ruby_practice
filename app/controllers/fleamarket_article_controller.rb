08e77088717041c49f10139d6040e49ae9221b33
comet <comet@daangn.com>
2024-01-08 18:26:48 +0900

    json 위주의 api 설계로 전향

--- app/controllers/fleamarket_article_controller.rb
+++ app/controllers/fleamarket_article_controller.rb
@@ -1,29 +1,41 @@
 class FleamarketArticleController < ApplicationController
   def index
-    @fleamarket_articles = FleamarketArticle.order(:id).last(10).reverse
+    articles = FleamarketArticle.order(:id).last(10).reverse
+
+    render json: articles, status: 200
   end
 
   def show
-    @article = FleamarketArticle.find_by(id: params[:id])
-
+    article = FleamarketArticle.find_by(id: params[:id])
     raise ActiveRecord::RecordNotFound.new if @article.nil? || @article.id.blank?
-
     FleamarketArticle::UpdateReadCountJob.perform_later(@article.id)
+
+    render json: article, status: 200
   end
 
-  def new
-    @article = Article.new
-  end
+  # ui 관련이므로 일단 뺌
+  # def new
+    # @article = FleamarketArticle.new
+  # end
 
   def create
-    FleamarketArticle::AddArticleJob.perform_later(user_id: 122,
-                                                   title: "...",
-                                                   content: "...",
-                                                   price: 100,
-                                                   trade_address: "hello",
-                                                   category: FleamarketArticle.categories[:etc_used])
+    FleamarketArticle::AddArticleJob.perform_later(fleamarket_article_params)
 
-    redirect_to fleamarket_articles_url
+    render status: 200
   end
 
+  def patch
+
+  end
+
+  def delete
+
+  end
+
+
+  private
+
+  def fleamarket_article_params
+    params.require(:article).permit(:user_id, :title, :content, :price, :trade_address, :category)
+  end
 end
